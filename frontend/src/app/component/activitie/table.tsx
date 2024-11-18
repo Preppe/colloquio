b1177162dd9af4141770a3d07db89666eee58abc
Giuseppe <preppe21@gmail.com>
2024-11-18 14:17:56 +0100

    end fe

--- frontend/src/app/component/activitie/table.tsx
+++ frontend/src/app/component/activitie/table.tsx
@@ -3,7 +3,7 @@ import { Activity } from "@/app/models/activity";
 import { useEffect, useState } from "react";
 import Chip from "../ui/chip";
 
-type DataRow = {
+export type DataRow = {
   project: string;
   employee: string;
   date: string;
@@ -17,7 +17,7 @@ const groupByKeys = (data: DataRow[], keys: string[]) => {
   if (!keys.length) {
     return data;
   }
-  return Object.values(
+  const ret = Object.values(
     data.reduce((acc, row) => {
       // Genera una chiave unica in base alle chiavi specificate
       const key = keys.map((k) => row[k as keyof DataRow]).join("-");
@@ -32,11 +32,25 @@ const groupByKeys = (data: DataRow[], keys: string[]) => {
       return acc;
     }, {} as Record<string, GroupedRow>)
   );
+
+  // inserisco hours come ultimo elemento
+  return ret.map((group) => {
+    const { hours, ...rest } = group;
+    return { ...rest, hours };
+  });
 };
 
 export function ActivityTable({ activities }: { activities: Activity[] }) {
-  const groups: (keyof DataRow)[] = [];
+  const [groups, setGroups] = useState<(keyof DataRow)[]>(['project', 'employee']);
   const [groupedActivities, setGroupedActivities] = useState<GroupedRow[]>([]);
+
+  const handleChangeGroups = (group: keyof DataRow) => {
+    if (groups.includes(group)) {
+      setGroups(groups.filter((g) => g !== group));
+    } else {
+      setGroups([...groups, group]);
+    }
+  }
 
   useEffect(() => {
     const dataRows: DataRow[] = activities.map((activity) => ({
@@ -46,11 +60,12 @@ export function ActivityTable({ activities }: { activities: Activity[] }) {
       hours: activity.hours,
     }));
     setGroupedActivities(groupByKeys(dataRows, groups));
-  }, []);
+  }, [activities, groups]);
 
   return (
     <>
-      <Chip />
+      <Chip isActive={groups.includes("project")} text="project" handleChangeGroups={handleChangeGroups} />
+      <Chip isActive={groups.includes("employee")} text="employee" handleChangeGroups={handleChangeGroups} />
       <div className="relative overflow-x-auto">
         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -65,9 +80,10 @@ export function ActivityTable({ activities }: { activities: Activity[] }) {
           <tbody>
             {groupedActivities.map((group, index) => (
               <tr key={index}>
-                {Object.values(group).map((value, index) => (
-                  <td key={index} className="px-6 py-4">
-                    {value}
+               {/* ciclo sul gruppo aggiungengo l eccezione per project e employee che lo mappo con il nome */}
+                {Object.entries(group).map(([key, value]) => (
+                  <td key={key} className="px-6 py-4">
+                    {key === "project" || key === "employee" ? activities.find((a) => a[key].id === value)?.[key].name : value}
                   </td>
                 ))}
               </tr>
